da0ea906db62f49528285e2ec36cf40241b5ab39
imillanm <israel.imillanm@gmail.com>
2023-09-20 14:58:05 -0300

    lab-challenge-terraform

---
+++ ec2/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.12.0"
+    }
+  }
+}
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+resource "aws_instance" "ec2_example" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_pair
+  security_groups = var.security_groups
+  tags = {
+    Name = var.instance_name
+  }
+}
